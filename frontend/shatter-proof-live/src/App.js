f0a7ea64c7149b83862676e64f2eb0a66799817f
EngerThen <engerthen4d17@gmail.com>
2019-09-27 22:19:13 -0400

    added routing

--- frontend/shatter-proof-live/src/App.js
+++ frontend/shatter-proof-live/src/App.js
@@ -1,43 +1,14 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Test} from './Test'
+import { Router, Link } from "@reach/router";
+import {Home} from './components/routes'
 
 class App extends Component {
   
-  state = {
-    name: ''
-  }
-  componentDidMount() {
-      const request = JSON.stringify({
-        test: 'trg'
-      });
-      fetch('http://127.0.0.1:5000/examplePost', {
-        method: 'POST',
-        body: request 
-      }).then(res => {
-        console.log(res.json())
-      })
-  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {JSON.stringify(this.state.name)}
-        </p>
-        <Test/>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+    <Router>
+      <Home path="/" />
+    </Router>
     );
   }
 }
---
+++ frontend/shatter-proof-live/src/components/routes/Home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+class Home extends Component {
+
+    state = {
+        name: ''
+    }
+    componentDidMount() {
+        const request = JSON.stringify({
+            test: 'trg'
+        });
+        fetch('http://127.0.0.1:5000/examplePost', {
+            method: 'POST',
+            body: request
+        }).then(res => {
+            console.log(res.json())
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        {JSON.stringify(this.state.name)}
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+        </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export {Home}
