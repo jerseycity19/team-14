c67894987aa889c5bc9b60fe5548e26f33524696
717wchen <53426416+717wchen@users.noreply.github.com>
2019-09-28 00:11:48 -0400

    Create Usercard.js
    Usercard template

---
+++ frontend/shatter-proof-live/src/components/routes/home/Usercard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+
+function Usercard (props){
+    return (
+        <div class="row">
+        <div class="col s12 m6">
+          <div class="card green-white darken-2">
+            <div class="card-content white-text">
+              <span class="card-title">Last name, First name</span>
+              <p> User's basic information</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+        </div>
+    );
+}
