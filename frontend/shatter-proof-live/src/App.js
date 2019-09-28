5e258fefdeea5c9869a648eafc836c9042383059
EngerThen <engerthen4d17@gmail.com>
2019-09-27 20:49:02 -0400

    added container

--- frontend/shatter-proof-live/src/App.js
+++ frontend/shatter-proof-live/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
   async componentDidMount() {
     try {
-      const response = await fetch('/examplePost', {
+      const response = await fetch('http://127.0.0.1:5000/examplePost', {
         method: 'POST',
         headers: {
           'Content- Type': 'application / json'
