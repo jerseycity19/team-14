168f00b5ac4fa2deeb6318d59f9bc22f35f5e2b1
EngerThen <engerthen4d17@gmail.com>
2019-09-27 20:13:54 -0400

    added fetch method

--- frontend/shatter-proof-live/src/App.js
+++ frontend/shatter-proof-live/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Test} from './Test'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  
+  state = {
+    name: ''
+  }
+  async componentDidMount() {
+    try {
+      const response = await fetch('/examplePost', {
+        method: 'POST',
+        headers: {
+          'Content- Type': 'application / json'
+        },
+        body: JSON.stringify({
+          test: 'trg'
+        })
+      });
+      const json = response.json();
+      console.log(json);
+      console.log("this is true");
+      this.setState({ name: json })
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <Test/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
